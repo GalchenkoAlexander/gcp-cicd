8a3361ba9f6b2f562a2ad74aaad99eda16eabb88
GalchenkoAlexander <galchenko.as@gmail.com>
2019-11-20 18:46:41 +0200

    import dag to composer

--- airflow/dags/sample-dag-hive-persistent.py
+++ airflow/dags/sample-dag-hive-persistent.py
@@ -7,14 +7,11 @@ from airflow.contrib.operators.dataproc_operator import DataProcHiveOperator
 
 from airflow.models import Variable
 
-PROJECT_ID = 'gcp-cicd-artifacts'
+PROJECT_ID = 'gcp-cicd'
 REGION_ID = 'us-central1'
 
-INPUT_BUCKET = 'gs://' + 'gcp-cicd-artifacts' + '/hive/input'
-OUTPUT_BUCKET = 'gs://' + 'gcp-cicd-artifacts' + '/hive/output'
-
-HQL_BUCKET = 'gs://' + PROJECT_ID + '/hive/hql/'
-UDF_BUCKET = 'gs://' + PROJECT_ID + '/hive/udf/'
+HQL_BUCKET = 'gs://' + PROJECT_ID + '-artifacts/hive/hql/'
+UDF_BUCKET = 'gs://' + PROJECT_ID + '-artifacts/hive/udf/'
 HQL_SCRIPT_NAME = 'input_tables.hql'
 UDF_JAR_MANE = 'gcp-cicd-udf-1.0-SNAPSHOT.jar'
 
@@ -36,21 +33,11 @@ with DAG(dag_id='hive-query-submit',
          start_date=start_date,
          schedule_interval=None) as dag:
 
-    # run_cluster_task = DataprocClusterCreateOperator(
-    #     task_id='HiveWorkflow',
-    #     project_id=PROJECT_ID,
-    #     region=REGION_ID,
-    #     # properties={
-    #     #     'autoscalingAlgorithm': 'THROUGHPUT_BASED',
-    #     #     'maxNumWorkers': '3',
-    #     # }
-    # )
-
     submit_hive_task = DataProcHiveOperator(
         task_id='HiveSubmit',
         project_id='gcp-cicd',
         cluster_name='cluster-1',
-        query_uri='gs://gcp-cicd-artifacts/hive/hql/input_tables.hql',
+        query_uri=HQL_BUCKET + HQL_SCRIPT_NAME,
         # dataproc_hive_jars=[UDF_BUCKET + UDF_JAR_MANE],
         # variables={'PROJECT_ID': PROJECT_ID},
         region='europe-west1'
