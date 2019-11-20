87ca795ceb39d1e59fd8c350a0816b937daadd53
GalchenkoAlexander <galchenko.as@gmail.com>
2019-11-20 18:47:11 +0200

    Scripts added

---
+++ scripts/run-build-import-dags-to-composer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+. ./env.sh
+cd ..
+
+### run build
+gcloud builds submit \
+--substitutions=\
+COMPOSER_ENV_NAME=${COMPOSER_ENV_NAME},\
+COMPOSER_REGION=${COMPOSER_REGION},\
+SOURCE=./airflow/dags/sample-dag-hive-persistent.py \
+--config ./cloudbuilds/cloudbuild-dag-import-to-composer-persistent.yaml .
+
