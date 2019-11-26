dafe98b0137e6181224e1b708df47909281d57a1
GalchenkoAlexander <galchenko.as@gmail.com>
2019-11-26 16:56:06 +0200

    CI/CD: Dataproc init actions autoscaling

---
+++ scripts/ci_init_action_run_cloudbuild_as.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source ./env.sh
+
+gcloud builds submit \
+--config=../cloudbuilds/ci_init_actions_as_dataproc_cluster.yaml \
+--substitutions=\
+_CLUSTER_NAME=gcp-cicd-dataproc-cluster,\
+_REGION=us-central1,\
+_ZONE=us-central1-b,\
+_BUILD_BUCKET=${ROOT_BUILD_PATH}
