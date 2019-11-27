48697aee1b71ca5fa658599dd6f0f72cc8e22b70
GalchenkoAlexander <galchenko.as@gmail.com>
2019-11-27 13:31:14 +0200

    CI/CD: Dataproc init actions autoscaling

--- scripts/ci_init_action_run_cloudbuild_as.sh
+++ scripts/ci_init_action_run_cloudbuild_as.sh
@@ -8,7 +8,7 @@ gcloud builds submit \
 _CLUSTER_NAME=gcp-cicd-dataproc-cluster,\
 _REGION=us-central1,\
 _ZONE=us-central1-b,\
-_BUCKET_NAME=${BUCKET_NAME}
-_REPO_NAME=${REPO_NAME}
-_BRANCH_NAME=${BRANCH_NAME}
-_SHORT_SHA=${SHORT_SHA}
+_BUCKET_NAME=${BUCKET_NAME},\
+REPO_NAME=${REPO_NAME},\
+BRANCH_NAME=${BRANCH_NAME},\
+SHORT_SHA=${SHORT_SHA}
--- scripts/env.sh
+++ scripts/env.sh
@@ -6,12 +6,7 @@ export PROJECT_ID='gcp-cicd'
 export BUCKET_NAME='gcp-cicd-artifacts'
 export REPO_NAME='gcp-cicd'
 export BRANCH_NAME='master'
-export SHORT_SHA=$('$PROJECT_ID' | md5)
-#export SHORT_SHA=$(date | md5)
-
-export ROOT_BUILD_PATH=${BUCKET_NAME}/${REPO_NAME}/${BRANCH_NAME}/${SHORT_SHA}
-
-#gcloud config set project ${PROJECT_ID}
+export SHORT_SHA=$(date | md5)
 
 touch ./env-persional.sh
 . ./env-persional.sh
