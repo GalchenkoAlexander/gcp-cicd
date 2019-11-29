9905b88d4f92f117c8bec3bd84916838f731aec7
GalchenkoAlexander <galchenko.as@gmail.com>
2019-11-29 15:22:08 +0200

    COMMIT_SHA instead on SHORT_SHA

--- scripts/dataproc/ci_init_action_run_cloudbuild_multi_node.sh
+++ scripts/dataproc/ci_init_action_run_cloudbuild_multi_node.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-source ../env.sh
-cd ../..
-gcloud builds submit \
---config=./cloudbuilds/ci_init_actions_multi_node_dataproc_cluster.yaml \
---substitutions=\
-_CLUSTER_NAME=gcp-cicd-dataproc-cluster,\
-_REGION=us-central1,\
-_ZONE=us-central1-b,\
-_BUCKET_NAME=${BUCKET_NAME},\
-REPO_NAME=${REPO_NAME},\
-BRANCH_NAME=${BRANCH_NAME},\
-SHORT_SHA=${SHORT_SHA}
+gcloud builds submit --config=./cloudbuilds/ci_init_actions_multi_node_dataproc_cluster.yaml \
+--substitutions=_CLUSTER_NAME=gcp-cicd-dataproc-cluster,_REGION=us-central1,_ZONE=us-central1-b,\
+_BUCKET_NAME=gcp-cicd-artifacts,REPO_NAME=gcp-cicd,BRANCH_NAME=master,COMMIT_SHA=778899
