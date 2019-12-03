c80d792382e25a514f0d81d4f921641c32cddc09
GalchenkoAlexander <galchenko.as@gmail.com>
2019-12-03 15:19:55 +0200

    CI/CD: Dataproc init actions Single cloudbuild file

--- scripts/dataproc/ci_init_action_run_cloudbuild_single_node.sh
+++ scripts/dataproc/ci_init_action_run_cloudbuild_single_node.sh
@@ -2,4 +2,4 @@
 
 gcloud builds submit --config=./cloudbuilds/ci_init_actions_single_node_dataproc_cluster.yaml \
 --substitutions=_CLUSTER_NAME=gcp-cicd-dataproc-cluster,_REGION=us-central1,_ZONE=us-central1-b,\
-_BUCKET_NAME=gcp-cicd-artifacts,REPO_NAME=gcp-cicd,BRANCH_NAME=master,COMMIT_SHA=778899
+_BUCKET_NAME=gcp-cicd-artifacts,REPO_NAME=gcp-cicd,BRANCH_NAME=master,COMMIT_SHA=778899,_CLUSTER_TYPE=single-node
